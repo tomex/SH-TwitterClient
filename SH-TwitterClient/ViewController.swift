531a2717ec9bf331c6b82224f8143b1f5b3cf160
tomex <tomex@tmx3.net>
2016-05-16 14:37:32 +0900

    Initial Commit

---
+++ SH-TwitterClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SH-TwitterClient
+//
+//  Created by guest on 2016/05/16.
+//  Copyright © 2016年 tmx3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SH-TwitterClientTests/SH_TwitterClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  SH_TwitterClientTests.swift
+//  SH-TwitterClientTests
+//
+//  Created by guest on 2016/05/16.
+//  Copyright © 2016年 tmx3. All rights reserved.
+//
+
+import XCTest
+@testable import SH_TwitterClient
+
+class SH_TwitterClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
